4a27152a0d5a3015b4a772dda88b72353a9fd453
Hurlyburly2 <dougdougmann@gmail.com>
2019-08-10 04:11:30 -0400

    added env for key. also customized api request

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
+
 const request = require('request')
 
-// const url goes here
+const url = 'https://api.darksky.net/forecast/' + process.env.DARKSKYKEY + '/37.8267,-122.4233'
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.currently)
+request({ url: url, json: true }, (error, response) => {
+    const currentData = response.body.currently
+    console.log(response.body.daily.data[0].summary + " It is currently " + currentData.temperature + " degrees out. There is a " + currentData.precipProbability + "% chance of rain.")
 })
