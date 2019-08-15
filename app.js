e07b9a4d615b6c99e22723db43ca91793939c7cf
Hurlyburly2 <dougdougmann@gmail.com>
2019-08-15 02:04:38 -0400

    restructured weather app slightly

--- app.js
+++ app.js
@@ -8,16 +8,16 @@ const location = process.argv[2]
 if (!location) {
     console.log("Please provide an address or location")
 } else {
-    geocode(location, (error, locationData) => {
+    geocode(location, (error, { latitude, longitude, location }) => {
         if (error) {
             return console.log(error)
         }
-        forecast(locationData.latitude, locationData.longitude, (error, forecastData) => {
+        forecast(latitude, longitude, (error, forecastData) => {
             if (error) {
                 return console.log(error)
             }
             
-            console.log(locationData.location)
+            console.log(location)
             console.log(forecastData)
         })
     })
--- utils/forecast.js
+++ utils/forecast.js
@@ -3,13 +3,13 @@ const request = require('request')
 const forecast = (latitude, longitude, callback) => {
   const url = 'https://api.darksky.net/forecast/' + process.env.DARKSKYKEY + '/' + latitude + ',' + longitude
 
-  request({ url: url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, { body }) => {
     if (error) {
       callback('Unable to connect to weather service', undefined)
-    } else if (response.body.error) {
+    } else if (body.error) {
       console.log("Unable to retrieve weather information", undefined)
     } else {
-      const forecast = response.body.daily.data[0].summary + " It is currently " + response.body.currently.temperature + " degrees out. There is a " + response.body.currently.precipProbability + "% chance of rain"
+      const forecast = body.daily.data[0].summary + " It is currently " + body.currently.temperature + " degrees out. There is a " + body.currently.precipProbability + "% chance of rain"
       callback(undefined, forecast)
     }
   })
